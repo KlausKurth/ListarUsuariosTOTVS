7a29178b64ce6ba073abcbbb54738c9803168b1e
KlausKurth <klausckurth@gmail.com>
2025-12-27 13:06:26 -0300

    FEAT estilização header e footer

--- 1_app/src/components/Header.jsx
+++ 1_app/src/components/Header.jsx
@@ -1,7 +1,26 @@
+import { NavLink } from "react-router-dom"
+
 function Header() {
     return(
         <div className="container-header">
-            <h1>Header</h1>                
+
+            <NavLink className="logo" to="/">{ '</1App>' }</NavLink>
+            
+            <nav className="nav-bar">
+                {
+                    localStorage.getItem('@1qpp/displayname') === null
+                    ?
+                    <>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to='/login'>Login</NavLink>
+                    </>
+                    :
+                    <>
+                    <NavLink to="/">Users</NavLink>
+                    <NavLink to="/logout">Logout</NavLink>
+                    </>
+                }
+            </nav>                
         </div>
     )
 }
